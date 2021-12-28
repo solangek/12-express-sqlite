f9b7fd1ebb07f480c8b351ec988ddea6dd2b0f3d
Sol <sodanabe@gmail.com>
2021-12-28 10:30:13 +0200

    uploading webstorm config

--- routes/api.js
+++ routes/api.js
@@ -38,7 +38,7 @@ router.post('/contacts', (req, res) => {
     return db.Contact.create({ firstName, lastName, phone })
         .then((contact) => res.send(contact))
         .catch((err) => {
-            console.log('***There was an error creating a contact', JSON.stringify(contact))
+            console.log('*** error creating a contact', JSON.stringify(contact))
             return res.status(400).send(err)
         })
 });
